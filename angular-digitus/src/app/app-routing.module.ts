7f275a3f08fe7382fd4648b0b02e78aee4a2283a
dew <dew@dream.local>
2020-07-08 20:19:28 +0300

    check guard added

--- angular-digitus/src/app/app-routing.module.ts
+++ angular-digitus/src/app/app-routing.module.ts
@@ -7,15 +7,16 @@ import { ForgotpasswordComponent } from './auth/forgotpassword/forgotpassword.co
 import { RenewpasswordComponent } from './renewpassword/renewpassword.component';
 import { AdminsignupComponent } from './auth/adminsignup/adminsignup.component';
 import { AdminpageComponent } from './adminpage/adminpage.component';
-import { AuthGuard } from './auth/auth.guard'
+import { AuthGuard} from './auth/auth.guard';
+import {  CheckGuard } from './auth/check.guard';
 
 
 const routes: Routes = [
   {path:"home",component:HomeComponent},
-  {path:"sign-up",component:SignupComponent},
-  {path:"login",component:LoginComponent},
+  {path:"sign-up",component:SignupComponent,canActivate:[CheckGuard]},
+  {path:"login",component:LoginComponent,canActivate:[CheckGuard]},
   {path:"forgot-password",component:ForgotpasswordComponent},
-  {path:"admin-signup",component:AdminsignupComponent},
+  {path:"admin-signup",component:AdminsignupComponent,canActivate:[CheckGuard]},
   {path:"adminpage",component:AdminpageComponent,canActivate:[AuthGuard]},
   {path:"",redirectTo:'home', pathMatch:'full'},
   {path:"renew-password/:token",component:RenewpasswordComponent}
